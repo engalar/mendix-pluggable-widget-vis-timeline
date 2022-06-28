893aed30b6c1b6b6bf31bfe99fe083df671f05bf
engalar <engalar@gmail.com>
2022-06-28 19:10:04 +0800

    lint

--- src/VisTimeline.tsx
+++ src/VisTimeline.tsx
@@ -1,6 +1,6 @@
 import { VisTimelineContainerProps } from "../typings/VisTimelineProps";
 import { ValueStatus } from "mendix";
-import { Timeline, DataSet, TimelineOptions, DataItem } from 'vis-timeline/standalone';
+import { Timeline, DataSet, TimelineOptions, DataItem } from "vis-timeline/standalone";
 import { useEffect, useRef } from "react";
 import { useMemoizedFn, useMount } from "ahooks";
 
@@ -30,62 +30,53 @@ export default function (props: VisTimelineContainerProps) {
         if (props.entityGroup.status === ValueStatus.Available) {
             const groups = props.entityGroup.items?.map(d => ({
                 id: props.attName.get(d).value,
-                content: props.attName.get(d).value,
+                content: props.attName.get(d).value
             }));
 
             if (timeLineRef.current) {
                 timeLineRef.current.setGroups(new DataSet(groups));
             }
         }
-
-        return () => {
-        }
-    }, [props.entityGroup])
+    }, [props.entityGroup]);
 
     useEffect(() => {
         if (props.entityEvent.status === ValueStatus.Available) {
             const items: DataItem[] = props.entityEvent.items!.map((d, i) => ({
                 id: i,
                 group: props.attGroup.get(d).value!,
-                content:
-                    props.attTitle.get(d).value!,
+                content: props.attTitle.get(d).value!,
                 start: props.attStartOfEvent.get(d).value!,
                 end: props.attEndOfEvent.get(d).value!,
-                editable: true,
+                editable: true
             }));
 
             if (timeLineRef.current) {
                 const itemDataSet: DataSet = new DataSet(items);
                 itemDataSet.on("update", (_: any, { data: [item] }: any) => {
                     onItemChange(item);
-                })
+                });
                 timeLineRef.current.setItems(itemDataSet);
             }
         }
+    }, [props.entityEvent]);
 
-        return () => {
+    useEffect(() => {
+        if (timeLineRef.current) {
+            timeLineRef.current.off("rangechanged");
+            timeLineRef.current.on("rangechanged", ({ start, end }) => {
+                props.attStart?.setValue(start);
+                props.attEnd?.setValue(end);
+            });
         }
-    }, [props.entityEvent])
-
-    useEffect(
-        () => {
-            if (timeLineRef.current) {
-                timeLineRef.current.off('rangechanged');
-                timeLineRef.current.on('rangechanged', ({ start, end }) => {
-                    props.attStart?.setValue(start);
-                    props.attEnd?.setValue(end);
-                })
-            }
-        }, [props.attStart, props.attEnd, timeLineRef.current],
-    )
+    }, [props.attStart, props.attEnd, timeLineRef.current]);
 
     useMount(() => {
-        var groups = new DataSet();
-        var items = new DataSet();
+        const groups = new DataSet();
+        const items = new DataSet();
 
         // create visualization
 
-        var options: TimelineOptions = {
+        const options: TimelineOptions = {
             groupOrder: "content", // groupOrder can be a property name or a sorting function
             // locale: 'zh-cn',
             editable: {
@@ -93,31 +84,33 @@ export default function (props: VisTimelineContainerProps) {
                 remove: true,
                 updateGroup: false,
                 updateTime: true,
-                overrideItems: true,
+                overrideItems: true
             },
             onUpdate(item, cb) {
-                console.log(item, 'onUpdate');
+                console.log(item, "onUpdate");
                 // cb(item);
             }
         };
 
         timeLineRef.current = new Timeline(ref.current, items, groups, options);
 
-        timeLineRef.current.on('select', ({ items, event }) => {
+        timeLineRef.current.on("select", ({ items, event }) => {
             console.log(items, event);
         });
     });
 
     useEffect(() => {
-        if (props.attStart && props.attEnd && props.attStart.status === ValueStatus.Available && props.attEnd.status === ValueStatus.Available) {
+        if (
+            props.attStart &&
+            props.attEnd &&
+            props.attStart.status === ValueStatus.Available &&
+            props.attEnd.status === ValueStatus.Available
+        ) {
             if (timeLineRef.current) {
                 timeLineRef.current.setWindow(props.attStart.value, props.attEnd.value);
             }
         }
-    }, [props.attStart, props.attEnd, timeLineRef.current])
+    }, [props.attStart, props.attEnd, timeLineRef.current]);
 
-    return (
-        <div className={props.class} style={props.style} ref={ref}>
-        </div>
-    );
+    return <div className={props.class} style={props.style} ref={ref}></div>;
 }
